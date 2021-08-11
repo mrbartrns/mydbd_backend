855b47759ce9c73c291397499511f9edb2227826
Junhyuck <mrbartrns@naver.com>
2021-08-11 16:46:46 +0900

    Install simple jwt token blacklist

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    # 'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'accounts',
 ]
