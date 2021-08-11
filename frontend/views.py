52ddcc57977909724c2fe72709bb471e0bc6054e
Junhyuck <mrbartrns@naver.com>
2021-08-11 23:07:08 +0900

    Make frontend static files and templates

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+class IndexTemplateView(TemplateView):
+    template_name = 'frontend/index.html'
