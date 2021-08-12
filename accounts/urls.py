65d6fb4d5bd3c3e5a4c41294d9c6e5d66f699163
Junhyuck <mrbartrns@naver.com>
2021-08-13 00:33:07 +0900

    Change url for api

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ from .views import *
 
 urlpatterns = [
     path('signup', UserRegisterView.as_view(), name='signup'),
-    path('api/login', CustomTokenObtainPairView.as_view(), name='token-login'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
-    path('api/token/verify', TokenVerifyView.as_view(), name='token-verify'),
-    path('api/logout', UserLogoutView.as_view(), name='token-logout'),
+    path('login', CustomTokenObtainPairView.as_view(), name='token-login'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify', TokenVerifyView.as_view(), name='token-verify'),
+    path('logout', UserLogoutView.as_view(), name='token-logout'),
     path('validate', ValidateSimpleJwtTokenView.as_view(), name='token-validate')
 ]
