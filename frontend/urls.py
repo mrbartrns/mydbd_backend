fe67f7920a008c67ae5c5a98f9c16d9db74ebbd9
Junhyuck <mrbartrns@naver.com>
2021-08-12 18:35:34 +0900

    Remove frontend app

--- frontend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from .views import *
-
-urlpatterns = [
-    re_path('', IndexTemplateView.as_view(), name='index')
-]
--- frontend/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-# Create your views here.
-class IndexTemplateView(TemplateView):
-    template_name = 'frontend/index.html'
