3831bdf063e9bbb3759dc24cb53af4d4ee05e81b
Junhyuck <mrbartrns@naver.com>
2021-08-16 16:31:12 +0900

    Modify admin page

--- posts/admin.py
+++ posts/admin.py
@@ -3,7 +3,26 @@ from django.contrib.admin import ModelAdmin
 
 from .models import *
 
+
 # Register your models here.
-admin.site.register(Killer)
-admin.site.register(Survivor)
-admin.site.register(Owner)
+
+
+class OwnerModelInline(admin.StackedInline):
+    model = Owner
+    can_delete = False
+
+
+class KillerAdmin(ModelAdmin):
+    list_display = ['id', 'name', 'name_kor', 'dt_created', 'dt_modified']
+    list_display_links = ['id', 'name', 'name_kor']
+    inlines = (OwnerModelInline,)
+
+
+class SurvivorAdmin(ModelAdmin):
+    list_display = ['id', 'name', 'name_kor', 'dt_created', 'dt_modified']
+    list_display_links = ['id', 'name', 'name_kor']
+    inlines = (OwnerModelInline,)
+
+
+admin.site.register(Killer, KillerAdmin)
+admin.site.register(Survivor, SurvivorAdmin)
