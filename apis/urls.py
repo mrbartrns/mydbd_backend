ee738035564b5922fbd926a1a5ab0bbacdf7896d
Junhyuck <mrbartrns@naver.com>
2021-08-30 23:28:21 +0900

    Add detail view

--- apis/urls.py
+++ apis/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list/killers', KillerListView.as_view(), name="killer-list-view"),
-    path('list/survivors', SurvivorListView.as_view(), name='survivor-list-view'),
-    path('list/perks', PerkListView.as_view(), name='perk-list-view'),
-    path('list/items', ItemListView.as_view(), name='item-list-view'),
-    # path('list/addons', ItemAddonListView.as_view(), name='item-addon-list-view'),
-
+    path('list/killers', KillerListView.as_view(), name="killer-list"),
+    path('list/survivors', SurvivorListView.as_view(), name='survivor-list'),
+    path('list/perks', PerkListView.as_view(), name='perk-list'),
+    path('list/items', ItemListView.as_view(), name='item-list'),
+    path('list/addons', ItemAddonListView.as_view(), name='item-addon-list-view'),
+    path('detail/killer/<int:killer_id>', KillerDetailView.as_view(), name='killer-detail'),
+    path('detail/survivor/<int:survivor_id>', SurvivorDetailView.as_view(), name='survivor-detail'),
+    path('detail/perk/<int:perk_id>', PerkDetailView.as_view(), name='perk-detail'),
+    path('detail/item/<int:item_id>', ItemDetailView.as_view(), name='item-detail'),
+    path('detail/item-addon/<int:addon_id>', ItemAddonDetailView.as_view(), name='item_addon-detail')
 ]
--- apis/views.py
+++ apis/views.py
@@ -66,3 +66,50 @@ class ItemAddonListView(APIView):
         page_number = request.GET.get('page', 1)
         page = paginator.get_page(page_number)
         return Response(ItemAddonSerializer(page, many=True).data, status=HTTP_200_OK)
+
+
+# TODO: after adding comments, HAVE TO REFACTOR THESE
+# TODO: make popularity now regarding to click
+class KillerDetailView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def get(self, request, killer_id):
+        killer = Killer.objects.get(id=killer_id)
+        return Response(KillerSerializer(killer).data, status=HTTP_200_OK)
+
+
+class SurvivorDetailView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def get(self, request, survivor_id):
+        survivor = Survivor.objects.get(id=survivor_id)
+        return Response(SurvivorSerializer(survivor).data, status=HTTP_200_OK)
+
+
+class PerkDetailView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def get(self, request, perk_id):
+        perk = Perk.objects.get(id=perk_id)
+        return Response(PerkSerializer(perk).data, status=HTTP_200_OK)
+
+
+class ItemDetailView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, item_id):
+        item = Item.objects.get(id=item_id)
+        return Response(ItemSerializer(item).data, status=HTTP_200_OK)
+
+
+class ItemAddonDetailView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def get(self, request, addon_id):
+        addon = ItemAddon.objects.get(id=addon_id)
+        return Response(ItemAddonSerializer(addon).data, status=HTTP_200_OK)
