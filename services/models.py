0b433fe8875b92d81a269f6d3f789c5472ff976f
Junhyuck <mrbartrns@naver.com>
2021-08-31 16:04:32 +0900

    Add service comment model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from apis.models import Category
+
+
 # Create your models here.
+# comment can be deleted by author, admin or staff
+class Comment(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='author')
+    parent_comment = models.OneToOneField('self', on_delete=models.SET_NULL, null=True, blank=True,
+                                          verbose_name='parent-comment')
+    category = models.OneToOneField(Category, on_delete=models.CASCADE, verbose_name='comment-category')
+    content = models.TextField(verbose_name='content')
+    dt_created = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+    dt_modified = models.DateTimeField(auto_now=True, verbose_name='수정일')
