b12546fe5c276ccecc3ca9b29414a918653e59aa
Junhyuck <mrbartrns@naver.com>
2021-09-02 16:07:29 +0900

    Alter service model category related name

--- services/models.py
+++ services/models.py
@@ -11,7 +11,8 @@ class Comment(models.Model):
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
                                verbose_name='parent-comment', related_name='children')
     depth = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='comment-category')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='comment-category',
+                                 related_name='comments')
     content = models.TextField(verbose_name='content')
     dt_created = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
     dt_modified = models.DateTimeField(auto_now=True, verbose_name='수정일')
