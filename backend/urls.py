8f7f2ab24e14e13c167524d60c0b4c1911a38a8b
Junhyuck <mrbartrns@naver.com>
2021-09-06 17:20:36 +0900

    Change url backend.py

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api/user/', include('accounts.urls')),
+                  path('user/', include('accounts.urls')),
                   path('api/', include('apis.urls')),
                   path('service/', include('services.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
