7313025fe7c8ed138d8931aa15dbe823cba8e3bf
junhyuck Ko <mrbartrns@naver.com>
2021-09-07 17:50:58 +0900

    add is_staff key on User serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -43,7 +43,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "email")
+        fields = ("id", "username", "email", "is_staff")
 
 
 # CUSTOMIZING JSON RESPONSE SERIALIZER
