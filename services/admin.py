0df77bb88d29aad3a551a36bee63f3fe80d0d5a0
Junhyuck Ko <mrbartrns@naver.com>
2021-11-08 17:51:57 +0900

    Register model on admin page

--- services/admin.py
+++ services/admin.py
@@ -6,7 +6,21 @@ from .models import *
 # Register your models here.
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'parent', 'depth', 'category', 'dt_created', 'dt_modified')
-    list_display_links = ('author',)
-    list_filter = ('dt_created', 'depth')
-    search_fields = ('author', 'content')
+    list_display = (
+        "author",
+        "parent",
+        "depth",
+        "category",
+        "dt_created",
+        "dt_modified",
+    )
+    list_display_links = ("author",)
+    list_filter = ("dt_created", "depth")
+    search_fields = ("author", "content")
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("user", "category", "comment", "like", "dislike")
+    list_display_links = ("user", "category", "comment")
+    list_filter = ("category", "comment")
