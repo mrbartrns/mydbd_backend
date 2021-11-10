4f5a155f1c84e804d2df4c2e58544da9484c942e
Junhyuck Ko <mrbartrns@naver.com>
2021-11-10 19:27:04 +0900

    Add article on like model

--- services/models.py
+++ services/models.py
@@ -36,36 +36,6 @@ class Comment(models.Model):
         return f"Comment by {self.author}"
 
 
-class Like(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name="user", related_name="likes"
-    )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-        verbose_name="like-category",
-        related_name="likes",
-        null=True,
-    )
-    comment = models.ForeignKey(
-        Comment,
-        on_delete=models.CASCADE,
-        verbose_name="like-comment",
-        related_name="likes",
-        null=True,
-    )
-    like = models.BooleanField(default=False)
-    dislike = models.BooleanField(default=False)
-    dt_created = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
-    dt_modified = models.DateTimeField(auto_now=True, verbose_name="수정일")
-
-    class Meta:
-        ordering = ["id"]
-
-    def __str__(self) -> str:
-        return f"{self.user.username} likes comment id  {self.comment if self.comment else self.category.id}"
-
-
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=20)
 
@@ -113,3 +83,40 @@ class ArticleTag(models.Model):
 
     def __str__(self) -> str:
         return f"Article object with tag name {self.tag.name if self.tag else 'None'}, article title {self.article.title if self.article else 'None'}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="user", related_name="likes"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="like-category",
+        related_name="likes",
+        null=True,
+    )
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+        verbose_name="like-comment",
+        related_name="likes",
+        null=True,
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        verbose_name="like-article",
+        related_name="likes",
+        null=True,
+    )
+    like = models.BooleanField(default=False)
+    dislike = models.BooleanField(default=False)
+    dt_created = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
+    dt_modified = models.DateTimeField(auto_now=True, verbose_name="수정일")
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self) -> str:
+        return f"{self.user.username} likes comment id  {self.comment if self.comment else self.category.id}"
