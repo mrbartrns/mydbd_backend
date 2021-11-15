10d5724b965c78447b9489298bba0ac2ab0cfdc6
Junhyuck Ko <mrbartrns@naver.com>
2021-11-15 17:11:26 +0900

    Add post tag url

--- services/urls.py
+++ services/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     ),
     path("comment/<int:pk>", CommentUpdateAndDeleteView.as_view()),
     path("comment/<int:pk>/like", CommentLikeView.as_view()),
+    path("search/tag", TagSearchView.as_view()),
     path("forum/<int:pk>/like", ArticleLikeView.as_view()),
     path("forum/list", ArticleListView.as_view()),
     path("forum/<int:pk>", ArticleDetailView.as_view()),
