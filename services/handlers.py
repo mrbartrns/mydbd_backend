ddf9b8b9b09ff85fe69e805e40b9135ea345233e
Junhyuck Ko <mrbartrns@naver.com>
2021-12-16 15:10:17 +0900

    work: resize image before save

--- services/handlers.py
+++ services/handlers.py
@@ -1,7 +1,9 @@
-from django.db.models.signals import post_save
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-
-from .models import Comment
+from io import BytesIO
+from PIL import Image as PILImage
+from .models import Comment, Image
 
 
 @receiver(post_save, sender=Comment)
@@ -10,6 +12,41 @@ def check_comment_depth(sender, instance, created, **kwargs):
         # instance.depth = instance.parent.depth + 1
         instance.depth = 1
         instance.save()
+
+
+@receiver(pre_save, sender=Image)
+def resize_image(sender, instance, **kwargs):
+    resize_width = 780
+
+    image = PILImage.open(instance.image)
+    fmt = image.format
+    if fmt == "gif":
+        return
+    image_width, image_height = image.size
+
+    width = 0
+    height = 0
+
+    # resize only when image width is over resize_width
+    if image_width > resize_width:
+        width = resize_width
+        height = (image_height * resize_width) // image_width
+    # resize image
+    image = image.resize((width, height), PILImage.ANTIALIAS)
+
+    # 메모리 상에서 byte 데이터를 처리하는 BytesIO 클래스 사용
+    buffer = BytesIO()
+    image.save(buffer, format="png")
+
+    file = InMemoryUploadedFile(
+        buffer,
+        "{}".format(instance.image),
+        "{}".format(instance.image),
+        "image/png",
+        buffer.tell(),
+        None,
+    )
+    instance.image = file
 
 
 # @receiver(post_save, sender=Comment)
--- services/models.py
+++ services/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image as pil
 
 from apis.models import Category
 
